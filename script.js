942adc8747db515950bfeef37e86beabd2c5c3da
hibenji <65447501+hibenji@users.noreply.github.com>
2020-08-29 23:55:55 +0200

    update

--- script.js
+++ script.js
@@ -98,39 +98,102 @@ function clearTimer() {
   document.getElementById('clear').style.display="none";
 }
 
+
+
+
+T1 = {} ;
+T1.timerDiv = document.getElementById('timer1');
+
+function displayTimer1() {
+  // initilized all local variables:
+  var hours1='00', minutes1='00',
+  miliseconds1=0, seconds1='00',
+  time1 = '',
+  timeNow1 = new Date().getTime(); // timestamp (miliseconds)
+
+  T1.difference = timeNow - T1.timerStarted;
+
+  // milliseconds
+  if(T1.difference > 10) {
+    miliseconds1 = Math.floor((T1.difference % 1000) / 10);
+    if(miliseconds1 < 10) {
+      miliseconds1 = '0'+String(miliseconds1);
+    }
+  }
+  // seconds
+  if(T1.difference > 1000) {
+    seconds1 = Math.floor(T1.difference / 1000);
+    if (seconds1 > 60) {
+      seconds1 = seconds1 % 60;
+    }
+    if(seconds1 < 10) {
+      seconds1 = '0'+String(seconds1);
+    }
+  }
+
+  // minutes
+  if(T1.difference > 60000) {
+    minutes1 = Math.floor(T1.difference/60000);
+    if (minutes1 > 60) {
+      minutes1 = minutes1 % 60;
+    }
+    if(minutes1 < 10) {
+      minutes1 = '0'+String(minutes1);
+    }
+  }
+
+  // hours
+  if(T1.difference > 3600000) {
+    hours1 = Math.floor(T1.difference/3600000);
+    // if (hours > 24) {
+    // 	hours = hours % 24;
+    // }
+    if(hours1 < 10) {
+      hours1 = '0'+String(hours1);
+    }
+  }
+
+  time1  =  hours1   + ':'
+  time1 += minutes1 + ':'
+  time1 += seconds1 + ':'
+  time1 += miliseconds1;
+
+  T1.timerDiv.innerHTML = time1;
+}
+
 function startTimer1() {
   // save start time
-  T.timerStarted = new Date().getTime()
+  T1.timerStarted = new Date().getTime()
   console.log('T.timerStarted: '+T.timerStarted)
 
   if (T.difference > 0) {
-    T.timerStarted = T.timerStarted - T.difference
+    T1.timerStarted = T1.timerStarted - T1.difference
   }
   // update timer periodically
-  T.timerInterval = setInterval(function() {
-    displayTimer()
+  T1.timerInterval = setInterval(function() {
+    displayTimer1()
   }, 10);
 
   // show / hide the relevant buttons:
-  document.getElementById('go').style.display="none";
-  document.getElementById('stop').style.display="inline";
-  document.getElementById('clear').style.display="none";
+  document.getElementById('go1').style.display="none";
+  document.getElementById('stop1').style.display="inline";
+  document.getElementById('clear1').style.display="none";
 }
 
 function stopTimer1() {
-  clearInterval(T.timerInterval); // stop updating the timer
+  clearInterval(T1.timerInterval); // stop updating the timer
 
-  document.getElementById('stop').style.display="none";
-  document.getElementById('go').style.display="inline";
-  document.getElementById('clear').style.display="inline";
+  document.getElementById('stop1').style.display="none";
+  document.getElementById('go1').style.display="inline";
+  document.getElementById('clear1').style.display="inline";
 }
 
 function clearTimer1() {
-  clearInterval(T.timerInterval);
-  T.timerDiv.innerHTML = "00:00:00:00"; // reset timer to all zeros
-  T.difference = 0;
+  clearInterval(T1.timerInterval);
+  T1.timerDiv.innerHTML = "00:00:00:00"; // reset timer to all zeros
+  T1.difference = 0;
 
-  document.getElementById('stop').style.display="none";
-  document.getElementById('go').style.display="inline";
-  document.getElementById('clear').style.display="none";
+  document.getElementById('stop1').style.display="none";
+  document.getElementById('go1').style.display="inline";
+  document.getElementById('clear1').style.display="none";
 }
