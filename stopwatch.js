227381f506cd7b4c08d3d5299e79dfff0c323702
hibenji <65447501+hibenji@users.noreply.github.com>
2020-08-29 23:39:44 +0200

    New stopwatch

--- stopwatch.js
+++ stopwatch.js
@@ -1,98 +1,99 @@
-//	Simple example of using private variables
-//
-//	To start the stopwatch:
-//		obj.start();
-//
-//	To get the duration in milliseconds without pausing / resuming:
-//		var	x = obj.time();
-//
-//	To pause the stopwatch:
-//		var	x = obj.stop();	// Result is duration in milliseconds
-//
-//	To resume a paused stopwatch
-//		var	x = obj.start();	// Result is duration in milliseconds
-//
-//	To reset a paused stopwatch
-//		obj.stop();
-//
-var	clsStopwatch = function() {
-		// Private vars
-		var	startAt	= 0;	// Time of last start / resume. (0 if not running)
-		var	lapTime	= 0;	// Time on the clock when last stopped in milliseconds
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 
-		var	now	= function() {
-				return (new Date()).getTime(); 
-			}; 
- 
-		// Public methods
-		// Start or resume
-		this.start = function() {
-				startAt	= startAt ? startAt : now();
-			};
+// global object
+T = {} ;
+T.timerDiv = document.getElementById('timer');
 
-		// Stop or pause
-		this.stop = function() {
-				// If running, update elapsed time otherwise keep it
-				lapTime	= startAt ? lapTime + now() - startAt : lapTime;
-				startAt	= 0; // Paused
-			};
+function displayTimer() {
+  // initilized all local variables:
+  var hours='00', minutes='00',
+  miliseconds=0, seconds='00',
+  time = '',
+  timeNow = new Date().getTime(); // timestamp (miliseconds)
 
-		// Reset
-		this.reset = function() {
-				lapTime = startAt = 0;
-			};
+  T.difference = timeNow - T.timerStarted;
 
-		// Duration
-		this.time = function() {
-				return lapTime + (startAt ? now() - startAt : 0); 
-			};
-	};
+  // milliseconds
+  if(T.difference > 10) {
+    miliseconds = Math.floor((T.difference % 1000) / 10);
+    if(miliseconds < 10) {
+      miliseconds = '0'+String(miliseconds);
+    }
+  }
+  // seconds
+  if(T.difference > 1000) {
+    seconds = Math.floor(T.difference / 1000);
+    if (seconds > 60) {
+      seconds = seconds % 60;
+    }
+    if(seconds < 10) {
+      seconds = '0'+String(seconds);
+    }
+  }
 
-var x = new clsStopwatch();
-var $time;
-var clocktimer;
+  // minutes
+  if(T.difference > 60000) {
+    minutes = Math.floor(T.difference/60000);
+    if (minutes > 60) {
+      minutes = minutes % 60;
+    }
+    if(minutes < 10) {
+      minutes = '0'+String(minutes);
+    }
+  }
 
-function pad(num, size) {
-	var s = "0000" + num;
-	return s.substr(s.length - size);
+  // hours
+  if(T.difference > 3600000) {
+    hours = Math.floor(T.difference/3600000);
+    // if (hours > 24) {
+    // 	hours = hours % 24;
+    // }
+    if(hours < 10) {
+      hours = '0'+String(hours);
+    }
+  }
+
+  time  =  hours   + ':'
+  time += minutes + ':'
+  time += seconds + ':'
+  time += miliseconds;
+
+  T.timerDiv.innerHTML = time;
 }
 
-function formatTime(time) {
-	var h = m = s = ms = 0;
-	var newTime = '';
+function startTimer() {
+  // save start time
+  T.timerStarted = new Date().getTime()
+  console.log('T.timerStarted: '+T.timerStarted)
 
-	h = Math.floor( time / (60 * 60 * 1000) );
-	time = time % (60 * 60 * 1000);
-	m = Math.floor( time / (60 * 1000) );
-	time = time % (60 * 1000);
-	s = Math.floor( time / 1000 );
-	ms = time % 1000;
+  if (T.difference > 0) {
+    T.timerStarted = T.timerStarted - T.difference
+  }
+  // update timer periodically
+  T.timerInterval = setInterval(function() {
+    displayTimer()
+  }, 10);
 
-	newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2) + ':' + pad(ms, 3);
-	return newTime;
+  // show / hide the relevant buttons:
+  document.getElementById('go').style.display="none";
+  document.getElementById('stop').style.display="inline";
+  document.getElementById('clear').style.display="none";
 }
 
-function show() {
-	$time = document.getElementById('time');
-	update();
+function stopTimer() {
+  clearInterval(T.timerInterval); // stop updating the timer
+
+  document.getElementById('stop').style.display="none";
+  document.getElementById('go').style.display="inline";
+  document.getElementById('clear').style.display="inline";
 }
 
-function update() {
-	$time.innerHTML = formatTime(x.time());
-}
+function clearTimer() {
+  clearInterval(T.timerInterval);
+  T.timerDiv.innerHTML = "00:00:00:00"; // reset timer to all zeros
+  T.difference = 0;
 
-function start() {
-	clocktimer = setInterval("update()", 1);
-	x.start();
-}
-
-function stop() {
-	x.stop();
-	clearInterval(clocktimer);
-}
-
-function reset() {
-	stop();
-	x.reset();
-	update();
+  document.getElementById('stop').style.display="none";
+  document.getElementById('go').style.display="inline";
+  document.getElementById('clear').style.display="none";
 }
