7f0fd9abc04f5e9a30ef00db0a1752d95bdd8ca0
hibenji <65447501+hibenji@users.noreply.github.com>
2020-08-30 01:43:50 +0200

    update

--- script.js
+++ script.js
@@ -9,29 +9,10 @@ T1.timerDiv = document.getElementById('timer1');
 function displayTimer() {
   // initilized all local variables:
   var hours='00', minutes='00',
-  miliseconds=0, seconds='00',
   time = '',
   timeNow = new Date().getTime(); // timestamp (miliseconds)
 
   T.difference = timeNow - T.timerStarted;
-
-  // milliseconds
-  if(T.difference > 10) {
-    miliseconds = Math.floor((T.difference % 1000) / 10);
-    if(miliseconds < 10) {
-      miliseconds = '0'+String(miliseconds);
-    }
-  }
-  // seconds
-  if(T.difference > 1000) {
-    seconds = Math.floor(T.difference / 1000);
-    if (seconds > 60) {
-      seconds = seconds % 60;
-    }
-    if(seconds < 10) {
-      seconds = '0'+String(seconds);
-    }
-  }
 
   // minutes
   if(T.difference > 60000) {
@@ -57,8 +38,6 @@ function displayTimer() {
 
   time  =  hours   + ':'
   time += minutes + ':'
-  time += seconds + ':'
-  time += miliseconds;
 
   T.timerDiv.innerHTML = time;
 }
@@ -92,7 +71,7 @@ function stopTimer() {
 
 function clearTimer() {
   clearInterval(T.timerInterval);
-  T.timerDiv.innerHTML = "00:00:00:00"; // reset timer to all zeros
+  T.timerDiv.innerHTML = "00:00"; // reset timer to all zeros
   T.difference = 0;
 
   document.getElementById('stop').style.display="none";
